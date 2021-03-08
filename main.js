9d6e80170abb38c64f2dbf6581d9b1cdb399ccd7
wvwall <waltervelardo@gmail.com>
2021-03-08 15:01:24 +0100

    Completo,da sistemare codice

--- main.js
+++ main.js
@@ -19,7 +19,60 @@ function reverse() {
         console.log("Questa parola non è palindroma");
     }
 }
-
 reverse();
 
-//Controllare se parola inserita è palindroma
+
+//PARI E DISPARI
+
+//chiedi all'untente pari o dispari
+var pariDispari = prompt("p=pari, d=dispari: ");
+switch (pariDispari) {
+    case "p":
+      console.log("Hai scelto pari.");  
+    break;
+    case "d":
+        console.log("Hai scelto dispari");
+    default:
+        console.log("Non hai inserito quanto richiesto!");
+    break;
+}
+
+// inserisci un numero da 1 a 5 
+var numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5:"));
+console.log(numeroUtente);
+
+
+// crea funzione per generare numeri
+function numeroRandom() {
+    var min = 1
+    var max = 5
+     numeroRan = Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log(numeroRan);
+}
+
+numeroRandom ();
+
+// sommiamo i due numeri
+
+var sommaNum = numeroUtente + numeroRan;
+console.log(sommaNum);
+
+//dichiara chi ha vinto
+
+function risultato() {
+    if (sommaNum%2 == 0) {
+        sommaNum = "p";
+        console.log(sommaNum);
+        console.log("il numero è pari!");
+    }else {
+        sommaNum = "d";
+        console.log(sommaNum);
+        console.log("il numero è dispari!");
+    }
+    if (sommaNum === pariDispari) {
+        console.log("Hai vinto");
+    } else {
+        console.log("Hai perso!");
+    }
+}
+risultato();
